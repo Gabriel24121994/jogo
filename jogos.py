3b4a2b10230405533f582c881a854e519d31bdd6
Gabriel24121994 <gabriel_coelho.94@hotmail.com>
2020-12-27 04:02:07 -0300

    Jogos utilizando comandos e bibliotecas simples do Python 3

---
+++ jogos.py
@@ -0,0 +1,12 @@
+print("  *********************************************************************")
+print("  *********************** ESCOLHA O SEU JOGO!!! ***********************")
+print("  *********************************************************************\n")
+
+jogo = input("Digite 1 para o Teste Jedi ou 2 para o Jogo da Forca.\n")
+
+if jogo == "1":
+    import advinhacao
+elif jogo == "2":
+    import forca
+else:
+    print("Só aceitamos 1 ou 2 como resposta para escolher o Jogo!\n")
