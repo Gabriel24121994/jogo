85144125f9b7e1f1ebe1e418987cd6eadd865ee0
Gabriel24121994 <gabriel_coelho.94@hotmail.com>
2020-12-31 01:56:30 -0300

    Criando loops do jogo da forca

--- forca.py
+++ forca.py
@@ -1,3 +1,19 @@
-print("  *****************************************************************************")
-print("  *********************** BEM-VINDO AO JOGO DA FORCA!!! ***********************")
-print("  *****************************************************************************\n")
+print("*****************************************************************************")
+print("*********************** BEM-VINDO AO JOGO DA FORCA!!! ***********************")
+print("*****************************************************************************\n")
+
+palavra_secreta = "banana"
+enforcou = False
+acertou = False
+
+while not enforcou and not acertou:
+
+    chute = input("Qual a letra?")
+    index = 0
+
+    for letra in palavra_secreta:
+        if chute == letra:
+            print("Encontrei a letra {} na posição {}".format(letra, index))
+        index = index + 1
+    print("Jogando")
+print("Fim de jogo!")
